4099cf797f4fd1c43ad2fbfdaf0fdd00aacf2607
Suklit Darapisut <109949720+dewsuklit@users.noreply.github.com>
2024-07-01 05:52:30 +0700

    First commit

--- dist/4/4.js
+++ dist/4/4.js
@@ -2,18 +2,36 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.autocomplete = autocomplete;
 function autocomplete(search, items, maxResult) {
-    search = search.toLowerCase();
-    let startsWithSearch = [];
-    let containsSearch = [];
-    items.forEach((item) => {
-        let lowerCaseItem = item.toLowerCase();
-        if (lowerCaseItem.indexOf(search) === 0) {
-            startsWithSearch.push(item);
+    let result = [];
+    let indD = [];
+    search = search.toUpperCase();
+    for (let item of items) {
+        let itemUpper = item.toUpperCase();
+        if (itemUpper.includes(search)) {
+            let tmpInd = itemUpper.indexOf(search);
+            let tmpScore = tmpInd / item.length;
+            if (indD.length === 0) {
+                indD.push(tmpScore);
+                result.push(item);
+            }
+            else {
+                let flag = true;
+                for (let i = 0; i < indD.length; i++) {
+                    if (tmpScore < indD[i]) {
+                        result.splice(i, 0, item);
+                        indD.splice(i, 0, tmpScore);
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    result.push(item);
+                    indD.push(tmpScore);
+                }
+            }
         }
-        else if (lowerCaseItem.indexOf(search) > -1) {
-            containsSearch.push(item);
-        }
-    });
-    let result = startsWithSearch.concat(containsSearch);
+    }
     return result.slice(0, maxResult);
 }
+// let result1 = autocomplete("th", ["Mother", "Think", "Worthy", "Apple", "Android"], 2);
+// console.log(result1);
--- src/4/4.ts
+++ src/4/4.ts
@@ -1,21 +1,39 @@
 export function autocomplete(search: string, items: string[], maxResult: number): string[] {
-    search = search.toLowerCase();
+    let result: string[] = [];
+    let indD: number[] = [];
+    search = search.toUpperCase();
 
-    let startsWithSearch: string[] = [];
-    let containsSearch: string[] = [];
+    for (let item of items) {
+        let itemUpper = item.toUpperCase();
+        if (itemUpper.includes(search)) {
+            let tmpInd = itemUpper.indexOf(search);
+            let tmpScore = tmpInd / item.length;
 
-    items.forEach((item) => {
-        let lowerCaseItem = item.toLowerCase();
-        if (lowerCaseItem.indexOf(search) === 0) {
-            startsWithSearch.push(item);
-        } else if (lowerCaseItem.indexOf(search) > -1) {
-            containsSearch.push(item);
+            if (indD.length === 0) {
+                indD.push(tmpScore);
+                result.push(item);
+            } else {
+                let flag = true;
+                for (let i = 0; i < indD.length; i++) {
+                    if (tmpScore < indD[i]) {
+                        result.splice(i, 0, item);
+                        indD.splice(i, 0, tmpScore);
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    result.push(item);
+                    indD.push(tmpScore);
+                }
+            }
         }
-    });
-
-    let result: string[] = startsWithSearch.concat(containsSearch);
-
+    }
     return result.slice(0, maxResult);
 }
+
+// let result1 = autocomplete("th", ["Mother", "Think", "Worthy", "Apple", "Android"], 2);
+// console.log(result1);
+
 
 
